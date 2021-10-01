16069a044712bf2a830871f801ccd35d72fd62c8
ksj0818 <ksj071808@gmail.com>
2021-10-02 02:02:20 +0900

    이미지 랜덤으로 출력

---
+++ js/background.js
@@ -0,0 +1,14 @@
+const images = [
+  "0.jpg",
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+];
+
+// JS에서 HTML 태그 추가하기 
+const chosenImg = images[Math.floor(Math.random() * images.length)];
+const bgimg = document.createElement('img');
+bgimg.src = `img/${chosenImg}`;
+
+
+document.body.prepend(bgimg)
