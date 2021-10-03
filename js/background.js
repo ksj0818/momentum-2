ff2d9ee98ca099ec81592bb67390514f1e0a1664
ksj0818 <ksj071808@gmail.com>
2021-10-04 07:22:34 +0900

    CSS 수정

--- js/background.js
+++ js/background.js
@@ -1,14 +1,14 @@
 const images = [
   "0.jpg",
   "1.jpg",
-  "2.jpg",
-  "3.jpg",
+  "2.jpg"
 ];
 
 // JS에서 HTML 태그 추가하기 
 const chosenImg = images[Math.floor(Math.random() * images.length)];
 const bgimg = document.createElement('img');
 bgimg.src = `img/${chosenImg}`;
+bgimg.id = "bgimg";
 
-
-document.body.prepend(bgimg);
+// document.body.prepend(bgimg);
+document.body.appendChild(bgimg)
